a75ad3850441deb6671e13a8c4f512b5b955a6b8
David Morales Llucia <39275512+dml1994@users.noreply.github.com>
2021-04-18 21:06:27 +0200

    Update admin.py

--- ConcertBand/app/admin.py
+++ ConcertBand/app/admin.py
@@ -4,18 +4,39 @@ from django.utils.translation import ngettext
 from django.contrib import messages
 
 
-
+# Libro de Cuentas
 class LedgerAdmin(admin.ModelAdmin):
     list_display = ('name','initialBalance','currentBalance')
     exclude = ('currentBalance',)
-    actions = ['delete_selected']
+    actions = ['delete_ledgers']
 
+    @admin.action(description="Elimina los libros de cuentas seleccionados")
+    def delete_ledgers(self, request, queryset):
+        for obj in queryset:
+            obj.delete()
+        self.message_user(request, ngettext(
+            'El Libro de Cuentas ha sido eliminado con éxito',
+            'Los Libros de Cuentas han sido eliminados con éxito',
+            queryset,
+            ), messages.SUCCESS)
+
+# Ejercicio
 class PeriodAdmin(admin.ModelAdmin):
     list_display = ('name','initialBalance','currentBalance','ledger')
     exclude = ('initialBalance','currentBalance')
-    actions = ['delete_selected']
+    actions = ['delete_periods']
 
+    @admin.action(description="Elimina los ejercicios seleccionados")
+    def delete_periods(self, request, queryset):
+        for obj in queryset:
+            obj.delete()
+        self.message_user(request, ngettext(
+            'El Ejercicio ha sido eliminado con éxito',
+            'Los Ejercicios han sido eliminados con éxito',
+            queryset,
+            ), messages.SUCCESS)
 
+# Entrada
 class EntryAdmin(admin.ModelAdmin):
     list_display = ('concept','amount','date','initialBalance','endBalance','period')
     exclude = ('initialBalance','endBalance')
@@ -34,18 +55,98 @@ class EntryAdmin(admin.ModelAdmin):
             queryset,
             ), messages.SUCCESS)
 
+# Categoria de Partitura
+class SheetCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    actions = ['delete_sheets']
+    search_fields = ('name',)
 
-admin.site.register(ScoreGenre)
-admin.site.register(SheetCategory)
+    @admin.action(description="Elimina las categorías seleccionadas")
+    def delete_sheets(self, request, queryset):
+        for obj in queryset:
+            obj.delete()
+        self.message_user(request, ngettext(
+            'La Categoría ha sido eliminada con éxito',
+            'Las Categorías han sido eliminadas con éxito',
+            queryset,
+            ), messages.SUCCESS)
+
+# Estilos de Obra
+class ScoreGenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    actions = ['delete_genres']
+    search_fields = ('name',)
+
+    @admin.action(description="Elimina los estilos seleccionados")
+    def delete_genres(self, request, queryset):
+        for obj in queryset:
+            obj.delete()
+        self.message_user(request, ngettext(
+            'El Estilo ha sido eliminado con éxito',
+            'Los Estilos han sido eliminados con éxito',
+            queryset,
+            ), messages.SUCCESS)
+
+# Compositor
+class ComposerAdmin(admin.ModelAdmin):
+    list_display = ('name','surname')
+    actions = ['delete_composers']
+    search_fields = ('name','surname')
+
+    @admin.action(description="Elimina los compositores seleccionados")
+    def delete_composers(self, request, queryset):
+        for obj in queryset:
+            obj.delete()
+        self.message_user(request, ngettext(
+            'El Compositor ha sido eliminado con éxito',
+            'Los Compositores han sido eliminados con éxito',
+            queryset,
+            ), messages.SUCCESS)
+
+# Video
+class VideoMediaAdmin(admin.ModelAdmin):
+    list_display = ('url',)
+    actions = ['delete_videos']
+    search_fields = ('url',)
+
+    @admin.action(description="Elimina los vídeos seleccionados")
+    def delete_videos(self, request, queryset):
+        for obj in queryset:
+            obj.delete()
+        self.message_user(request, ngettext(
+            'El vídeo ha sido eliminado con éxito',
+            'Los Vídeos han sido eliminados con éxito',
+            queryset,
+            ), messages.SUCCESS)
+
+# Eventos
+class EventMediaAdmin(admin.ModelAdmin):
+    list_display = ('name','place','category','startDate','endDate')
+    actions = ['delete_events']
+    search_fields = ('name','place')
+
+    @admin.action(description="Elimina los eventos seleccionados")
+    def delete_events(self, request, queryset):
+        for obj in queryset:
+            obj.delete()
+        self.message_user(request, ngettext(
+            'El Evento ha sido eliminado con éxito',
+            'Los Eventos han sido eliminados con éxito',
+            queryset,
+            ), messages.SUCCESS)
+
+
+admin.site.register(ScoreGenre, ScoreGenreAdmin)
+admin.site.register(SheetCategory, SheetCategoryAdmin)
 admin.site.register(Ledger, LedgerAdmin)
 admin.site.register(Period, PeriodAdmin)
 admin.site.register(Entry, EntryAdmin)
-admin.site.register(Composer)
-admin.site.register(Video)
+admin.site.register(Composer, ComposerAdmin)
+admin.site.register(VideoMedia,VideoMediaAdmin)
 admin.site.register(Score)
 admin.site.register(Sheet)
 admin.site.register(Musician)
 admin.site.register(Material)
-admin.site.register(Event)
+admin.site.register(Event, EventMediaAdmin)
 
 admin.site.disable_action('delete_selected')
