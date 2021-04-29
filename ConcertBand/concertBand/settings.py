eaf50083596bcad21ec0fb90006f249aadd781dc
dml1994 <dml94@live.com>
2021-04-29 23:50:26 +0200

    listos

--- ConcertBand/concertBand/settings.py
+++ ConcertBand/concertBand/settings.py
@@ -131,7 +131,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+BASEURL = 'https://concertband.herokuapp.com'
 
+APIS = {}
+
+import django_heroku
+django_heroku.settings(locals())
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
