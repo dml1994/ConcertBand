4bfd75fa31160d648a833d84abc04c5a99cf048d
dml1994 <dml94@live.com>
2021-04-30 12:31:55 +0200

    heroku

--- ConcertBand/concertBand/settings.py
+++ ConcertBand/concertBand/settings.py
@@ -30,7 +30,7 @@ STATICFILES_DIRS = (
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-u0w19w7++j&=ch7v9y5igr(9@auwi$*be^g$c^yb**$)pl2#2z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
