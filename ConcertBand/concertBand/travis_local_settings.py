ba33712d74af9c3d68f871112f266dd9ead32aae
dml1994 <dml94@live.com>
2021-05-06 18:54:44 +0200

    local settings

--- ConcertBand/concertBand/travis_local_settings.py
+++ ConcertBand/concertBand/travis_local_settings.py
@@ -14,15 +14,4 @@ APIS = {
 
 BASEURL = 'http://localhost:8000'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('POSTGRESQL_NAME'),
-        'USER': env('POSTGRESQL_USER'),
-        'PASSWORD': env('POSTGRESQL_PASS'),
-        'HOST': env('POSTGRESQL_HOST'),
-        'PORT': env('POSTGRESQL_PORT'),
-    }
-}
-
 KEYBITS = 256
