1921eefdbd69b853019a7fb512372dd13caab84d
dmoralescover <dml94@live.com>
2021-06-29 17:14:28 +0200

    heroku

--- ConcertBand/concertBand/settings.py
+++ ConcertBand/concertBand/settings.py
@@ -158,6 +158,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 LOGIN_REDIRECT_URL = '/'
 
 
+import django_heroku
+django_heroku.settings(locals())
+
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
