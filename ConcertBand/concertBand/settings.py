b6e8ae0bb05542a4ec520210475734c8f3d9e1d7
dmoralescover <dml94@live.com>
2021-06-29 17:36:27 +0200

    heroku

--- ConcertBand/concertBand/settings.py
+++ ConcertBand/concertBand/settings.py
@@ -23,7 +23,7 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'staticfiles'),
 )
 
 # Quick-start development settings - unsuitable for production
